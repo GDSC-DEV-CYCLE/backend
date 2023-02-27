8aa585e9fe316f26ee46c7a9c97b694d59cbde42
Younghun Hwang <younghun.ai@gmail.com>
2023-02-27 19:24:45 +0900

    [feat] 댓글 생성, 조회 API

---
+++ server/src/main/java/devcycle/server/controller/CommentController.java
@@ -0,0 +1,31 @@
+package devcycle.server.controller;
+
+import devcycle.server.domain.comment.Comment;
+import devcycle.server.dto.CreateCommentDto;
+import devcycle.server.dto.comment.GetCommentDto;
+import devcycle.server.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping("/comment/post/{postId}")
+    public ResponseEntity<Boolean> createComment(@RequestHeader Map<String, String> requestHeader, @PathVariable Long postId, @RequestBody CreateCommentDto dto) {
+        commentService.createComment(requestHeader, postId, dto.getContent());
+        return ResponseEntity.ok().body(true);
+    }
+
+    @GetMapping("/comments/post/{postId}")
+    public ResponseEntity<List<GetCommentDto>> getCommentList(@PathVariable Long postId) {
+        return ResponseEntity.ok().body(commentService.getCommentList(postId));
+    }
+}
---
+++ server/src/main/java/devcycle/server/service/CommentService.java
@@ -0,0 +1,63 @@
+package devcycle.server.service;
+
+import devcycle.server.config.JwtTokenProvider;
+import devcycle.server.domain.Post;
+import devcycle.server.domain.PostRepository;
+import devcycle.server.domain.comment.Comment;
+import devcycle.server.domain.comment.CommentRepository;
+import devcycle.server.domain.like.LikesRepository;
+import devcycle.server.domain.user.User;
+import devcycle.server.domain.user.UserRepository;
+import devcycle.server.dto.comment.GetCommentDto;
+import io.jsonwebtoken.JwtException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class CommentService {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final CommentRepository commentRepository;
+
+    public Long createComment(Map<String, String> requestHeader, Long postId, String content) {
+        String accessToken = requestHeader.get("authorization").substring(7);
+        if (!jwtTokenProvider.validateToken(accessToken)) {
+            throw new JwtException("유효한 토큰이 아닙니다.");
+        }
+        Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+        User user;
+        Post post;
+        try {
+            user = userRepository.findByEmail(authentication.getName()).orElseThrow();
+        } catch (Exception e) {
+            throw new RuntimeException("존재하지 않는 회원입니다.");
+        }
+        try {
+            post = postRepository.findById(postId).orElseThrow();
+        } catch (Exception e) {
+            throw new RuntimeException("존재하지 않는 게시글입니다.");
+        }
+        Comment comment = Comment.builder().user(user).post(post).content(content).build();
+        return commentRepository.save(comment).getId();
+    }
+
+    public List<GetCommentDto> getCommentList(Long postId) {
+        List<Comment> commentList = commentRepository.findByPostId(postId);
+        List<GetCommentDto> commentDtoList = new ArrayList<>();
+        for (Comment comment : commentList) {
+            commentDtoList.add(GetCommentDto.builder().id(comment.getId()).userName(comment.getUser().getName()).content(comment.getContent()).build());
+        }
+        return commentDtoList;
+    }
+}
